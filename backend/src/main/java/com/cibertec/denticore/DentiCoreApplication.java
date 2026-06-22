bbec676f458484c6b664f78d8667561a0148d892
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-06-22 16:42:51 -0500

    chore: establecer nombre profesional DentiCoreApplication y actualizar pom

---
+++ backend/src/main/java/com/cibertec/denticore/DentiCoreApplication.java
@@ -0,0 +1,12 @@
+package com.cibertec.denticore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentiCoreApplication { // CamelCase correcto
+
+    public static void main(String[] args) {
+        SpringApplication.run(DentiCoreApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/com/cibertec/denticore/DentiCoreApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cibertec.denticore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentiCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
