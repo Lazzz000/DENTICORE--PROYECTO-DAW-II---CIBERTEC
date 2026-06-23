49353b3a2506303b34a200c34ff2af4339add0cf
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-06-22 20:51:26 -0500

    feat(security):DENTI-26 configuracion base de Spring Security, BCrypt y UserDetailsService

--- backend/src/main/java/com/cibertec/denticore/security/repositories/UsuarioRolRepository.java
+++ backend/src/main/java/com/cibertec/denticore/security/repositories/UsuarioRolRepository.java
@@ -2,8 +2,15 @@ package com.cibertec.denticore.security.repositories;
 
 import com.cibertec.denticore.security.entities.UsuarioRol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRolRepository extends JpaRepository<UsuarioRol, UsuarioRol.UsuarioRolId> {
+
+    @Query("SELECT ur FROM UsuarioRol ur JOIN FETCH ur.rol WHERE ur.id.idUsuario = :idUsuario AND ur.activo = true")
+    List<UsuarioRol> findByIdUsuario(@Param("idUsuario") Integer idUsuario);
 }
